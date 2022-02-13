7540b82b537d538cffa4bd7aaee63766ab1ad1ea
ThomasRmnd <92300828+ThomasRmnd@users.noreply.github.com>
2022-02-13 14:14:06 +0100

    creation d'une classe scene
    aller il faut continuer

--- include/scene.h
+++ include/scene.h
@@ -7,8 +7,18 @@ class scene
 {
 private:
 	object* objects;
-
+	unsigned int nb_objects;
 	lumiere* lumieres;
+	unsigned int nb_lumieres;
 
 public:
+	scene();
+	scene(const object*, const lumiere*);
+	scene(const scene&);
+	~scene();
+
+	scene& operator=(const scene&);
+
+	scene& add_object(const object&);
+	scene& add_lumieres(const lumiere&);
 };
---
+++ src/mycode/scene.cpp
@@ -0,0 +1,9 @@
+#include "scene.h"
+
+scene::scene()
+{
+	objects = NULL;
+	lumieres = NULL;
+	nb_objects = 0;
+	nb_lumieres = 0;
+}
